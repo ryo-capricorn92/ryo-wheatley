9a7a0bdf93b2b22fd442621827945e3cc51be896
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-12-20 14:51:13 -0500

    (feat) add tag canvas for skills bubble

---
+++ client/src/canvas.js
@@ -0,0 +1,18 @@
+/* global $ */
+$(document).ready(function () {
+  var width = ($('#skillsCanvasContainer').width() * 0.5) + 'px';
+  $('#skillsCanvas').attr('width', width).attr('height', width);
+  if (!$('#skillsCanvas').tagcanvas({
+    imageMode: 'image',
+    freezeActive: true,
+    clickToFront: 1,
+    maxSpeed: 0.03,
+    depth: 0.75
+  }, 'skillsTags')) {
+    console.error('Tag Canvas failed to load');
+    $('#skillsCanvasContainer').hide();
+  }
+  // $('#skillsCanvas span').each(function (i, el) {
+  //   $(el).attr('width', $(el).width() + 'px').attr('height', $(el).height() + 'px');
+  // });
+})
